72dc69e982311ba4c92c46941046369a201db40b
student <student@MacBook-Pro-admin-2.local>
2023-10-26 20:59:54 +0700

    second-lesson

---
+++ iosSchool_HH/Assembly.swift
@@ -0,0 +1,16 @@
+//
+//  Assembly.swift
+//  iosSchool_HH
+//
+//  Created by student on 26.10.2023.
+//
+
+import Foundation
+
+class Assembly {
+
+    func colorGenerator(alpha: Double) -> ColorGenerator {
+        ColorGenerator(alpha: alpha)
+    }
+
+}
--- iosSchool_HH/ViewController.swift
+++ iosSchool_HH/ViewController.swift
@@ -11,19 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var color = Color(red: 1, green: 1, blue: 1)
-        color.alpha = .bright
-
-        var nextColor = color
-        nextColor.alpha = .average
 
         let generator = ColorGenerator(alpha: 0.5)
-        generator.alpha = 1
+        let _: ColorGeneratorProtocol = ColorGenerator(alpha: 1)
 
-        let nextGenerator = generator
-        nextGenerator.alpha = 0
-
-        let alpha: Brightness = .average
-        let description = alpha.description()
     }
 }
