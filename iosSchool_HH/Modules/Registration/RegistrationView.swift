ddd33d447ef03f9043adcbf87416f0ab5d14303a
student <lerabubl@gmail.com>
2023-11-19 00:19:55 +0700

    buttons and textfield styled

--- iosSchool_HH/Modules/Registration/RegistrationView.swift
+++ iosSchool_HH/Modules/Registration/RegistrationView.swift
@@ -23,18 +23,29 @@ class RegistrationViewImp: UIView, RegistrationView {
     func setView() {
         userImageView.image = UIImage(named: "user-profile")
 
-        loginTextField = generateTextField()
-        passwordTextField = generateTextField()
-        repeatPswdTextField = generateTextField()
+        styleTextField(textField: loginTextField)
+        styleTextField(textField: passwordTextField)
+        styleTextField(textField: repeatPswdTextField)
+
+        styleButton(button: registrationButton)
+        styleButton(button: backButton)
     }
 
-    private func generateTextField() -> UITextField {
-        let textField = UITextField()
+    private func styleTextField(textField: UITextField) {
         textField.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
         textField.layer.cornerRadius = 15
         textField.layer.borderWidth = 1
         textField.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        textField.textColor = UIColor(red: 0.318, green: 0.306, blue: 0.306, alpha: 1)
+    }
 
-        return textField
+    private func styleButton(button: UIButton) {
+        button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        button.layer.shadowOpacity = 1
+        button.layer.shadowRadius = 8
+        button.layer.shadowOffset = CGSize(width: 0, height: 5)
+        button.layer.backgroundColor = UIColor(red: 0.329, green: 0.506, blue: 0.706, alpha: 1).cgColor
+        button.layer.cornerRadius = 10
+        button.titleLabel?.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
     }
 }
