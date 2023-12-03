f8ac3c033f01661e7b639b72c3207f9634c20313
student <lerabubl@gmail.com>
2023-12-03 16:39:04 +0700

    character module restored

---
+++ iosSchool_HH/Modules/Character/CharacterView.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterView.swift
+//  iosSchool_HH
+//
+//  Created by student on 16.11.2023.
+//
+
+import UIKit
+
+protocol CharacterView: UIView {
+}
+
+class CharacterViewImp: UIView, CharacterView {
+}
